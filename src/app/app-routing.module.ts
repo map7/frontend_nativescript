80fd832ba3730912e829ad82ca3cdfc2ef3687f9
Michael Pope <michael@dtcorp.com.au>
2019-10-01 13:52:19 +1000

    SignUp-FEAT-Add sign_up route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 const routes: Routes = [
     { path: "", redirectTo: "/login", pathMatch: "full" },
     { path: "login", loadChildren: () => import("~/app/login/login.module").then((m) => m.LoginModule) },
-    { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) }
+    { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) },
+    { path: "sign_up", loadChildren: () => import("~/app/sign_up/sign_up.module").then((m) => m.SignUpModule) }
 ];
 
 @NgModule({
