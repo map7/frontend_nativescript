470963908b095cbe2a01e03bd467f07ff137c86a
Michael Pope <michael@dtcorp.com.au>
2019-10-01 14:10:34 +1000

    Home-Test-Nest tests which are after login

--- e2e/sample.e2e-spec.ts
+++ e2e/sample.e2e-spec.ts
@@ -3,7 +3,7 @@ import { assert } from "chai";
 
 const addContext = require('mochawesome/addContext');
 
-describe("sample scenario", () => {
+describe("Login Screens", () => {
     let driver: AppiumDriver;
 
     before(async function(){
@@ -26,21 +26,7 @@ describe("sample scenario", () => {
         // Check heading
         const lblHeading = await driver.findElementByAccessibilityId("heading");
         assert.equal(await lblHeading.text(), "Login");
-    }); 
-
-    it("ends up on 'home' screen after login", async function () {
-        // Login
-        const btnLoginTap = await driver.findElementByAccessibilityId("btnLogin");
-        await btnLoginTap.click();
-        
-        // Check heading
-        const lblHeading = await driver.findElementByAccessibilityId("heading");
-        assert.equal(await lblHeading.text(), "Home");
-
-        // Logout
-        const btnLogoutTap = await driver.findElementByAccessibilityId("btnLogout");
-        await btnLogoutTap.click();
-    }); 
+    });
 
     it("ends up on 'sign up' screen after tapping Sign Up", async function () {
         // Login
@@ -52,9 +38,45 @@ describe("sample scenario", () => {
         assert.equal(await lblHeading.text(), "Sign Up");
 
         // Login
-        const btnLoginTap = await driver.findElementByAccessibilityId("btnLogin");
-        await btnLoginTap.click();
+        const btnLoginNavTap = await driver.findElementByAccessibilityId("btnLogin");
+        await btnLoginNavTap.click();
     }); 
+
+    describe("After login", () => {
+
+        beforeEach(async function () {
+            // Login
+            const btnLoginTap = await driver.findElementByAccessibilityId("btnLogin");
+            await btnLoginTap.click();
+        });
+        
+        afterEach(async function () {
+            // Nav to Login
+            const btnLoginNavTap = await driver.findElementByAccessibilityId("btnLogin");
+            await btnLoginNavTap.click();
+        });
+        
+        it("ends up on 'home' screen after login", async function () {
+            // Check heading
+            const lblHeading = await driver.findElementByAccessibilityId("heading");
+            assert.equal(await lblHeading.text(), "Home");
+        }); 
+
+    });
+
+    // it("ends up on 'manual' screen after tapping Manual", async function () {
+    //     // Login
+    //     const btnLoginTap = await driver.findElementByAccessibilityId("btnLogin");
+    //     await btnLoginTap.click();
+        
+    //     // Check heading
+    //     const lblHeading = await driver.findElementByAccessibilityId("heading");
+    //     assert.equal(await lblHeading.text(), "Home");
+
+    //     // Logout
+    //     const btnLogoutTap = await driver.findElementByAccessibilityId("btnLogout");
+    //     await btnLogoutTap.click();
+    // }); 
     
     // it("should find an element by text", async function () {
     //     const btnTap = await driver.findElementByAutomationText("TAP");
