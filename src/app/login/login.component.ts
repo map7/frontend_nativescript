3f86501cd60ab1aca32a4aac74489bca5351b104
Michael Pope <michael@dtcorp.com.au>
2019-10-01 12:50:19 +1000

    Login-FEAT-Create a method to navigate to /home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "Login",
     templateUrl: "./login.component.html"
 })
 export class LoginComponent implements OnInit {
-
-    constructor() {
+    constructor(private router: Router) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+    }
+
+    navHome() {
+        this.router.navigate(["/home"]);
     }
 }
