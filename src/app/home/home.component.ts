7039e28858b00d0266d68fbd1e66338e6827020c
Michael Pope <michael@dtcorp.com.au>
2019-10-01 15:31:54 +1000

    Feedback-FEAT-Add button to home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,4 +22,8 @@ export class HomeComponent implements OnInit {
     navManual() {
         this.router.navigate(["/manual"]);
     }
+
+    navFeedback() {
+        this.router.navigate(["/feedback"]);
+    }
 }
