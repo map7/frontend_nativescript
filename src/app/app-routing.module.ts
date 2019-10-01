10ca9e36e1497cadd565805ed12ff3db243f1f0f
Michael Pope <michael@dtcorp.com.au>
2019-10-01 15:23:07 +1000

    Manual-FEAT-Add a route for 'manual'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     { path: "", redirectTo: "/login", pathMatch: "full" },
     { path: "login", loadChildren: () => import("~/app/login/login.module").then((m) => m.LoginModule) },
     { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) },
+    { path: "manual", loadChildren: () => import("~/app/manual/manual.module").then((m) => m.ManualModule) },
     { path: "sign_up", loadChildren: () => import("~/app/sign_up/sign_up.module").then((m) => m.SignUpModule) }
 ];
 
