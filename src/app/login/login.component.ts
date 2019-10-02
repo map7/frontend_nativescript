016628fe1a8cd1f036cc8afd39bc0e222433aa91
Michael Pope <michael@dtcorp.com.au>
2019-10-02 16:15:32 +1000

    Devise-FEAT-Add HttpClient require libraries to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
+
 import * as localStorage from "@proplugins/nativescript-localstorage";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Component({
     selector: "Login",
     templateUrl: "./login.component.html"
 })
 export class LoginComponent implements OnInit {
-    constructor(private router: Router) {
+    constructor(private router: Router, private http: HttpClient) {
         // Use the component constructor to inject providers.
     }
 
