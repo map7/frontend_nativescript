9cfe70d109f0ee610f75816a55d6871529778b8a
Michael Pope <michael@dtcorp.com.au>
2019-10-02 16:15:05 +1000

    Devise-FEAT-Import HttpClientModule to be used everywhere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
+import { HttpClientModule } from "@angular/common/http";
+
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent
