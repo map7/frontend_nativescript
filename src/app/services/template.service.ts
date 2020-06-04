a883b31bfc12fe79b7fdc0cff36b149278d83467
Michael Pope <michael@dtcorp.com.au>
2020-06-04 14:02:32 +1000

    General-FEAT-Add a template for a service
    This is just a quick template which you can copy and start a nice
    little service.

---
+++ src/app/services/template.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+import { GlobalVariables } from "../global.variables";
+
+@Injectable()
+export class TemplateService {
+    constructor(private GlobalVariables: GlobalVariables) { }
+
+    // Add methods here.
+}
