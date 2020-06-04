aa0707928849ff97a8333798f5422fe34e11774d
Michael Pope <michael@dtcorp.com.au>
2020-06-04 14:57:45 +1000

    Home-FEAT-Call home page on backend (error)
    Currently I get a 401 Unauthorized error as I'm not saving the tokens
    and passing them back so the backend doesn't know if I'm
    authenticated.
    
    This works if you take out the 'before_action :authenticate_user!' in
    the backend controller as you don't need to sign in to access the page then.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+
+import { catchError, map, tap } from "rxjs/operators";
+
+import { GlobalVariables } from "../global.variables";
+
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private router: Router) {
+    public welcome: String;
+    
+    constructor(private router: Router,
+                private http: HttpClient,
+                private GlobalVariables: GlobalVariables) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+        let url = this.GlobalVariables.API_URL + "/home.json";
+
+        this.http.get(url,{observe: "response"})
+            .pipe(
+                map(res =>{
+                    console.log("Reponse: " + res.body["message"]);
+                    return res.body;
+                })
+            ).subscribe(body => {
+                this.welcome = body["message"];
+            });
     }
 
     navLogin() {
